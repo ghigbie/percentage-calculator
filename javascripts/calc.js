2ceeec72450ceaa556860e32959efa8445becebb
George Higbie <gehigbie@gmail.com>
2017-12-21 16:37:20 +0000

    Add javascript

--- javascripts/calc.js
+++ javascripts/calc.js
@@ -2,10 +2,12 @@ const numerator = document.getElementById('numerator'),
       denominator = document.getElementById('denominator'),
       form = document.getElementById('mainForm'),
       result = document.getElementById('result');
-      
-let calculatePercent = (numerator, denominator) => {
+  
+let calculatePercent = (numerator, denominator, targetElement) => {
     this.numerator = parseFloat(numerator.value);
     this.denominator = parseFloat(denominator.value);
-}
+    let percentResult = (this.numerator/this.denominator) * 100;
+    targetElement.innerHTML = percentResult;
+};
 
-form.addEventListener('submit', )
+form.addEventListener('submit', calculatePercent(numerator, denominator, result));
