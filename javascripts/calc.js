5b7c0d7d7cfc9e37bb623d82fe50e74fa3aae32a
George Higbie <gehigbie@gmail.com>
2017-12-21 15:20:48 +0000

    Add new styles

--- javascripts/calc.js
+++ javascripts/calc.js
@@ -2,3 +2,10 @@ const numerator = document.getElementById('numerator'),
       denominator = document.getElementById('denominator'),
       form = document.getElementById('mainForm'),
       result = document.getElementById('result');
+      
+let calculatePercent = (numerator, denominator) => {
+    this.numerator = parseFloat(numerator.value);
+    this.denominator = parseFloat(denominator.value);
+}
+
+form.addEventListener('submit', )
