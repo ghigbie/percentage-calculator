5a3d7f841a521714f66519238c9c5e37ce61cc56
George Higbie <gehigbie@gmail.com>
2017-12-21 15:05:01 +0000

    Add const variables to javascript file

---
+++ javascripts/calc.js
@@ -0,0 +1,4 @@
+const numerator = document.getElementById('numerator'),
+      denominator = document.getElementById('denominator'),
+      form = document.getElementById('mainForm'),
+      result = document.getElementById('result');
