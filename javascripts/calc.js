9c2d215de5c9bec3da1ba4be8be05121b7598e75
George Higbie <gehigbie@gmail.com>
2017-12-21 23:08:41 -0700

    Add properties to js file

--- javascripts/calc.js
+++ javascripts/calc.js
@@ -5,6 +5,37 @@ const dropdown    = document.getElementById('dropdownButton'),
       form        = document.getElementById('mainForm'),
       result      = document.getElementById('result');
 
+let option1 = {
+    id: 1,
+    name: 'What is Y% of X?',
+    formula: ""
+}
+
+let option2 = {
+    id: 2,
+    name: 'X is Y% of what?',
+    formula: ""
+}
+
+let option3 = {
+    id: 3,
+    name: 'What % of X is Y?',
+    formula: ""
+}
+
+let option4 = {
+    id: 4,
+    name: 'X% of what is Y?',
+    formula: ""
+}
+
+let option5 = {
+    id: 5,
+    name: 'Y% of X is what?',
+    formula: ""
+}
+
+
 form.addEventListener('submit', (event) => {
     let x = parseFloat(numerator.value);
     let y = parseFloat(denominator.value);
